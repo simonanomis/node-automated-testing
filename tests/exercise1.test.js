abd0ad80b2f8e229aa0d702b3d413e4975facc79
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-30 18:16:49 +0200

    fizz buzz exercise

---
+++ tests/exercise1.test.js
@@ -0,0 +1,26 @@
+const lib = require('../exercise1');
+
+describe('greet', () => {
+    it('should throw an exception if input is not a number', () => {
+        expect(() => { lib.fizzBuzz('a')}).toThrow();
+        expect(() => { lib.fizzBuzz(null)}).toThrow();
+        expect(() => { lib.fizzBuzz(undefined)}).toThrow();
+        expect(() => { lib.fizzBuzz({})}).toThrow();
+    });
+    it('should return FizzBuzz if input is divisible by 3 and 5', () => {
+        const result = lib.fizzBuzz(15);
+        expect(result).toBe('FizzBuzz');
+    });
+    it('should return Fizz if input is divisible by 3', () => {
+        const result = lib.fizzBuzz(3);
+        expect(result).toBe('Fizz');
+    });
+    it('should return Buzz if input is divisible by 3', () => {
+        const result = lib.fizzBuzz(5);
+        expect(result).toBe('Buzz');
+    });
+    it('should return input if input is not divisible by 3 or 5', () => {
+        const result = lib.fizzBuzz(1);
+        expect(result).toBe(1);
+    });
+});
