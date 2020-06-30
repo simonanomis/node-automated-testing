c49e9fc3298f868eee3895ebfa63c29bd0f6ad6a
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-30 17:19:40 +0200

    testing absolute numbers

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node-automated-testing
 NodeJS automated testing demo
+
+Install JEST https://jestjs.io/docs/en/getting-started: 
+npm i jest --save-dev
+
+Run:
+npm test
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,16 @@
-test('Our first test', () => {
-    throw new Error('something failed');
+const lib = require('../lib');
+
+test('absolute - should return positive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
 });
+
+test('absolute - should return positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});  
+
+test('absolute - should return 0 if input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+}); 
