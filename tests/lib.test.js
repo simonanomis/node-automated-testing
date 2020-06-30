6dda9fd2472c5f89afbd3380ea653cb952f84726
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-30 17:23:40 +0200

    group similar tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,19 @@
 const lib = require('../lib');
 
-test('absolute - should return positive number if input is positive', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
+describe('absolute', () => {
+    it('should return positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });  
+    
+    it('should return 0 if input is 0', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    }); 
 });
 
-test('absolute - should return positive number if input is negative', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});  
-
-test('absolute - should return 0 if input is 0', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
-}); 
